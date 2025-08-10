e3eb7287c76489e7ce52b3ffe003192ebedd8728
armeagle777 <tyeranyan@yahoo.com>
2025-08-11 03:42:44 +0400

    feat: helpers to fetch wp data

--- src/modules/wp/helpers.js
+++ src/modules/wp/helpers.js
@@ -7,70 +7,76 @@ const migrationAxiosInstance = axios.create({
   baseURL: process.env.MIGRATION_SERVICE_API_URL,
 });
 
-// const migrationApiHttpsAgent = new https.Agent({
-//   key: getRequestPrivateKey(),
-//   cert: getRequestCertificate(),
-//   rejectUnauthorized: false,
-// });
-
-// const fetchWpDataExample = async () => {
-//   const postData = JSON.stringify({
-//     [searchProp]: certificateNumber,
-//     date_from: "01/01/1970",
-//     ssn: "2721801111",
-//   });
-//   const axiosOptions = createAxiosOptionsWithSignature(postData);
-//   return migrationAxiosInstance(axiosOptions);
-// };
+const migrationApiHttpsAgent = new https.Agent({
+  key: getRequestPrivateKey(),
+  cert: getRequestCertificate(),
+  rejectUnauthorized: false,
+});
 
 const fetchCountriesData = async () => {
   return migrationAxiosInstance.get("/work-permit/countries");
 };
 
 const fetchFilterPersonData = async (body) => {
-  return migrationAxiosInstance.post("/work-permit/persons/filter", body);
+  const postData = JSON.stringify(body);
+  const axiosOptions = createAxiosOptionsWithSignature(
+    postData,
+    "/work-permit/persons/filter"
+  );
+  return migrationAxiosInstance(axiosOptions);
 };
 
 const fetchPersonFullData = async (id, body) => {
-  return migrationAxiosInstance.post(`/work-permit/persons/detail/${id}`, body);
+  const postData = JSON.stringify(body);
+  const axiosOptions = createAxiosOptionsWithSignature(
+    postData,
+    `/work-permit/persons/detail/${id}`
+  );
+  return migrationAxiosInstance(axiosOptions);
 };
 
 const fetchPersonWpLightData = async (pnum) => {
-  return migrationAxiosInstance.get(`/work-permit/persons/${pnum}`);
+  const postData = JSON.stringify({ pnum });
+  const axiosOptions = createAxiosOptionsWithSignature(
+    postData,
+    `/work-permit/persons/detail/pnum`
+  );
+
+  return migrationAxiosInstance(axiosOptions);
 };
 
-// function getRequestPrivateKey() {
-//   return fs.readFileSync("./src/certificates/migration-api.key", "utf8");
-// }
+function getRequestPrivateKey() {
+  return fs.readFileSync("./src/certificates/migration-request.key", "utf8");
+}
 
-// function getRequestCertificate() {
-//   return fs.readFileSync("./src/certificates/migration-api.pem", "utf8");
-// }
+function getRequestCertificate() {
+  return fs.readFileSync("./src/certificates/migration-request.pem", "utf8");
+}
 
-// function createSignature(postData) {
-//   const sign = crypto.createSign("RSA-SHA256");
-//   sign.update(postData);
-//   sign.end();
-//   const signature = sign.sign(privateKey, "base64");
-//   return signature;
-// }
+function createSignature(postData) {
+  const sign = crypto.createSign("RSA-SHA256");
+  sign.update(postData);
+  sign.end();
+  const signature = sign.sign(getRequestPrivateKey(), "base64");
+  return signature;
+}
 
-// function createAxiosOptionsWithSignature(postData, url) {
-//   const signature = createSignature(postData);
+function createAxiosOptionsWithSignature(postData, url) {
+  const signature = createSignature(postData);
 
-//   return {
-//     method: "post",
-//     url,
-//     headers: {
-//       "Content-Type": "application/json",
-//       "Content-Length": Buffer.byteLength(postData),
-//       "X-Signature-Algorithm": "RSA-SHA256",
-//       "X-Signature": signature,
-//     },
-//     data: postData,
-//     httpsAgent: migrationApiHttpsAgent,
-//   };
-// }
+  return {
+    method: "post",
+    url,
+    headers: {
+      "Content-Type": "application/json",
+      "Content-Length": Buffer.byteLength(postData),
+      "X-Signature-Algorithm": "RSA-SHA256",
+      "X-Signature": signature,
+    },
+    data: postData,
+    httpsAgent: migrationApiHttpsAgent,
+  };
+}
 
 module.exports = {
   fetchCountriesData,
